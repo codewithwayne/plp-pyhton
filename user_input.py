354bfdf644298337cb6e6587c5d4f004ecc46733
Wayne Imbuga Lumadede <144681210+codewithwayne@users.noreply.github.com>
2024-03-11 12:49:44 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name= input("Enter your name:")
+age= input("Enter your age:")
+location=input("Enter your loaction:")
+
+#print("Greetings", name )
+print("Greetings", name + ", you are", age + " years old and you live in", location)
